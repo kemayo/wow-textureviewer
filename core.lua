b0f55dbc00a559bfd43587a573aed2c2c197c48e
David Lynch <kemayo@gmail.com>
2015-11-29 14:52:54 -0600

    Input for SetTexCoord testing

--- core.lua
+++ core.lua
@@ -4,6 +4,7 @@ local myname, ns = ...
 -- [[Interface\Minimap\POIIcons]]
 -- "Interface\\ChatFrame\\ChatFrameBackground"
 -- "Interface\\Tooltips\\UI-Tooltip-Border"
+-- "Interface\\Glues\\CharacterCreate\\UI-CharacterCreate-Classes" 0, 0.25, 0, 0.25
 
 ns:RegisterEvent("ADDON_LOADED")
 function ns:ADDON_LOADED(event, addon)
@@ -17,7 +18,7 @@ end
 
 local frame = CreateFrame("Frame", myname, UIParent)
 frame:SetWidth(350)
-frame:SetHeight(90)
+frame:SetHeight(150)
 frame:SetPoint("CENTER")
 frame:SetBackdrop({ bgFile = "Interface\\ChatFrame\\ChatFrameBackground", edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border", edgeSize = 16, insets = { left = 3, right = 3, top = 3, bottom = 3 } })
 frame:SetBackdropColor(0, 0, 0, 0.8)
@@ -65,17 +66,68 @@ frame.input:SetBackdrop({
 frame.input:SetBackdropColor(0.1, 0.1, 0.2, 1)
 frame.input:SetBackdropBorderColor(0.8, 0.8, 0.9, 0.4)
 frame.input:SetPoint("TOPLEFT",12,-38)
-frame.input:SetPoint("BOTTOMRIGHT",-12,12)
+frame.input:SetPoint("TOPRIGHT",-12,-38)
+frame.input:SetHeight(40)
 
 frame.input:SetScript("OnEscapePressed", function(self) frame:Hide() end)
 frame.input:SetScript("OnTextChanged", function(self, by_user_input)
-    local path = self:GetText():gsub([[\\]], [[\]]):gsub("%[%[", ""):gsub("%]%]", ""):gsub([["]], "")
     -- to be nice, replace double-slashes
+    local path = self:GetText():gsub([[\\]], [[\]]):gsub("%[%[", ""):gsub("%]%]", ""):gsub([["]], "")
+
+    -- jiggle the size a bit to make it display at native size
     frame.texture:SetSize(0, 0)
     frame.texture:ClearAllPoints()
     frame.texture:SetPoint("TOPLEFT", frame, "TOPRIGHT", 4, 0)
 
     frame.texture:SetTexture(path)
+    frame.texcoords:Enable()
+
+    frame.texcoords:GetScript("OnTextChanged")(frame.texcoords, by_user_input)
+end)
+frame.input:SetScript("OnEnter", function(self)
+    GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+    GameTooltip:AddLine("Texture path")
+    GameTooltip:AddLine("Quotes and double-slashes will be handled automatically")
+    GameTooltip:Show()
+end)
+frame.input:SetScript("OnLeave", function()
+    GameTooltip:Hide()
+end)
+
+frame.texcoords = CreateFrame("EditBox", nil, frame)
+frame.texcoords:SetFontObject("GameFontHighlight")
+frame.texcoords:SetTextInsets(10, 10, 3, 3) -- left, right, top, bottom
+frame.texcoords:SetBackdrop({
+    bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
+    edgeFile = "Interface\\Tooltips\\UI-Tooltip-Border",
+    edgeSize = 16,
+    insets = { left = 4, right = 4, top = 4, bottom = 4, },
+})
+frame.texcoords:SetBackdropColor(0.1, 0.1, 0.2, 1)
+frame.texcoords:SetBackdropBorderColor(0.8, 0.8, 0.9, 0.4)
+frame.texcoords:SetPoint("TOPLEFT", frame.input, "BOTTOMLEFT", 0, -10)
+frame.texcoords:SetPoint("TOPRIGHT", frame.input, "BOTTOMRIGHT", 0, -10)
+frame.texcoords:SetHeight(frame.input:GetHeight())
+frame.texcoords:Disable()
+
+frame.texcoords:SetScript("OnEscapePressed", function(self) frame:Hide() end)
+frame.texcoords:SetScript("OnTextChanged", function(self, by_user_input)
+    local coords = { string.split(",", (self:GetText():gsub("%s", ""))) }
+    if 4 == #coords or 8 == #coords then
+        frame.texture:SetTexCoord(unpack(coords))
+    else
+        frame.texture:SetTexCoord(0, 1, 0, 1)
+    end
+end)
+frame.texcoords:SetScript("OnEnter", function(self)
+    GameTooltip:SetOwner(self, "ANCHOR_RIGHT")
+    GameTooltip:AddLine("Texture coordinates")
+    GameTooltip:AddLine("{left, right, top, bottom}")
+    GameTooltip:AddLine("{ULx, ULy, LLx, LLy, URx, URy, LRx, LRy}")
+    GameTooltip:Show()
+end)
+frame.texcoords:SetScript("OnLeave", function()
+    GameTooltip:Hide()
 end)
 
 frame.close = CreateFrame("Button", nil, frame, "UIPanelCloseButton")
